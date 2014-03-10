a43ce5fbeea1f649bed0e8b7386d61fea98957ed
Rahul Ramakrishnan <rahul@plaid.com>
2014-03-10 15:15:28 -0700

    Fixed terminal sample terminal list

--- Genetic_Programming_Rahul/data_scrape.py
+++ Genetic_Programming_Rahul/data_scrape.py
@@ -2,11 +2,20 @@
 #Custom module for pulling terminal data
 #from sources including: .csv file
 
+import random
 
 def get_Terminal():
-	terminal = [1, 2, 3, 4, 5, 6, 7, 8, 10]
+	terminal = [Open, High, Low, Volume, DOW, S&P]
+	#Add 10 random coefficients into terminal list
+	#to be used when applying operators on signals
+	for i in range(0,10):
+		random_integer = random.randInt(-10, 10)
+		termain.append(random_integer * random.random())	
+
 	return terminal
 
 def get_Functional():
-	functional = ['+' , '/', '%', '*', '-', '^']
+	#Need to add more operators
+	#like cube root 
+	functional = ['+' , '/', '*', '-', '^']
 	return functional
