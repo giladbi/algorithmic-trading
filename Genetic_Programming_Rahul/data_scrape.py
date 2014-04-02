e9f8b768105937a52d4d32837d4314be641ba4da
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-01 19:43:35 -0700

    Included more functional and terminal node options

--- Genetic_Programming_Rahul/data_scrape.py
+++ Genetic_Programming_Rahul/data_scrape.py
@@ -7,20 +7,25 @@ import random
 
 #Could use yield here
 def get_Terminal():
-	terminals = ['Open', 'High', 'Low', 'Volume', 'DOW', 'S&P']
+	terminals = ['apple_open', 'apple_high', 'apple_low', 
+		     'apple_close', 'apple_volume', 'dow_open', 
+		     'dow_high', 'dow_low', 'dow_close', 
+                     'dow_volume']
+
 	#Add 10 random coefficients into terminal list
 	#to be used when applying operators on signals
+
 	for i in range(0,10):
 		random_integer = random.randint(-5, 5)
 		terminals.append(random_integer * random.random())	
 
+	random.shuffle(terminals)	
 	return terminals
 
 def get_Functional():
 	#Need to add more operators
 	#like cube root 
-	functionals = ['+' , '/', '*', '-', '^']
+	functionals = ['+' , '/', '*', '-', '^', 'exp', 'log', 'ln']
+	random.shuffle(functionals)
 	return functionals
-
-
 
