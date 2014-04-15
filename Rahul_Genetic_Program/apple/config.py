8c63a6ea86ce7e8973319bac94189294dcdd1e49
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-15 15:22:31 -0700

    Changed mutation and crossover probability

--- Rahul_Genetic_Program/apple/config.py
+++ Rahul_Genetic_Program/apple/config.py
@@ -5,5 +5,5 @@ population_size = 30   	#Number of trees in the population
 tree_size = 8      	#Number of nodes in a tree
 generations = 10  	#Number of generations
 c_probability = .7 	#Crossover probability
-m_probability = .8 	#Mutation probability
+m_probability = .2 	#Mutation probability
 
