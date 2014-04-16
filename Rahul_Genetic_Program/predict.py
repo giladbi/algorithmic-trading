19b80b49cf098088e495ef19f1c4d9a7139ebb3a
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-15 17:41:04 -0700

    Consolidated output statements into one method

--- Rahul_Genetic_Program/predict.py
+++ Rahul_Genetic_Program/predict.py
@@ -1,24 +1,5 @@
 #Rahul Ramakrishnan
-#Main Script
-'''
-        ========================= To Do: =====================
-	0. Print out equation, fitness, best -----------------------------------------------DONE
-        1. Add more diversity preserving (Test each one)
-                a. Roullette Wheel ---------------------------------------------------------DONE
-                b. Tournament Selection ----------------------------------------------------DONE
-        2. Graph the evolution of the different methods
-        3. Add more terminal nodes (data from s&p500)
-        4. Add more functional nodes (^, cosine, sine, tangent, log, ln, exp (e^))
-        5. Convert for loops to map & reduce -----------------------------------------------DONE
-        6. Use filter & scan when you can --------------------------------------------------DONE
-        7. Become more memory efficient where possible 
-	8. Create training data ------------------------------------------------------------DONE
-	9. Separate modules in a package __init__.py ---------------------------------------DONE
-	10. Include parameter control
-	11. Include colors in the output ---------------------------------------------------DONE
-	12. Find out a way to choose functional nodes randomly -----------------------------DONE
-	13. Ensure that the size of the trees is between A and B ---------------------------DONE
-'''
+#Main Apple Stock Predicting Script
 
 from apple import config
 from apple import initialize
@@ -29,14 +10,11 @@ from apple import fitness
 
 from termcolor import colored
 
-
-#Initialize objects and literals
 population = initialize.initPopulation(config.population_size)
 
 for generation in xrange(0, config.generations):			
-	print (colored("\nGENERATION: %d", 'magenta') %(generation))
 	fitness.generateFitnesses(population)
-	inspect.printEquationPopulation(population)	
+	inspect.printEquationPopulation(population, generation)
 	for tree in xrange(0, len(population)):			
 		recombination.performMutation(population)
 		recombination.performCrossover(population)
