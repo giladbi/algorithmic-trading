3a5c0c5c260078ce3b07e08eff753bd727115c64
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-15 17:43:16 -0700

    Fixed default config parameters

--- Rahul_Genetic_Program/apple/config.py
+++ Rahul_Genetic_Program/apple/config.py
@@ -1,9 +1,9 @@
 #Rahul Ramakrishnan
 #module: config
 
-population_size = 30   	#Number of trees in the population
+population_size = 16   	#Number of trees in the population
 tree_size = 8      	#Number of nodes in a tree
-generations = 10  	#Number of generations
+generations = 20  	#Number of generations
 c_probability = .7 	#Crossover probability
 m_probability = .2 	#Mutation probability
 
