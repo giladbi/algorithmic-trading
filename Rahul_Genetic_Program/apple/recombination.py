94834b90220b9c03acf99cfc6b7bca37577b7126
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-19 14:07:12 -0700

    Added size of sub tree check for crossover, prevents pre-mature convergence

--- Rahul_Genetic_Program/apple/recombination.py
+++ Rahul_Genetic_Program/apple/recombination.py
@@ -23,26 +23,22 @@ def mutate(root):
 	#Choose to swap terminal or functional nodes
 	node_type = choice(['terminal', 'functional'])
 	if(node_type == 'terminal'): 
-		t_node_values = findRandomNodes(root,1)
+		t_node_values = getNodes(root,1)
 		v_1 = choice(t_node_values)
-		#remove from pool
-		t_node_values.remove(v_1)
+		t_node_values.remove(v_1)  #remove from pool
 		v_2 = choice(t_node_values)
-		#Retrieve node references
-		node_1 = DFS(root, v_1)
+		node_1 = DFS(root, v_1) #Retrieve node references
 		node_2 = DFS(root, v_2)
 		#print "Mutation: swapping %s with %s" %(node_1.value, node_2.value)
 		swapValues(node_1, node_2)
 	else: #type == 'functional'
-		f_node_values = findRandomNodes(root,2)
+		f_node_values = getNodes(root,2)
 		#Safeguards against a one element f_node
 		if (len(f_node_values) > 1):
-			v_1 = choice(f_node_values)
-			#remove from pool
-			f_node_values.remove(v_1)
+			v_1 = choice(f_node_values)	
+			f_node_values.remove(v_1) #remove from pool
 			v_2 = choice(f_node_values)
-			#Retrieve node references
-			node_1 = DFS(root, v_1)
+			node_1 = DFS(root, v_1) #Retrieve node references
 			node_2 = DFS(root, v_2)
 			swapValues(node_1, node_2)				
 
@@ -57,15 +53,11 @@ def performCrossover(population):
 		if(random() <= config.c_probability):
 			crossover(population[i].root, population[i+1].root)		
 
-def crossover(root_1, root_2):
 
-        #Find random functional node value in tree 1
-        f_1_nodes = findRandomNodes(root_1,3)	
-        node_1 = choice(f_1_nodes)
-        f_2_nodes = findRandomNodes(root_2,3)
-        node_2 = choice(f_2_nodes)
-        #Swap subtrees
-        swapValues(node_1, node_2)
+def crossover(root_1, root_2):
+	node_1 = chooseRandomSubTree(root_1, 3)
+	node_2 = chooseRandomSubTree(root_2, 3)
+        swapValues(node_1, node_2) #Swap subtrees
 	#print "Crossover: Swapping %s with %s" %(node_1.value, node_2.value)
         swapNodes(node_1, node_2)
 
@@ -73,7 +65,15 @@ def crossover(root_1, root_2):
 '''
 	Wrappers
 '''
-def findRandomNodes(root, t_or_f=0):	
+def chooseRandomSubTree(root, t_or_f):		
+	functional_nodes = getNodes(root_1,3)	
+	#Choose from sub-trees that have a depth greater than 2
+	#This prevents pre-mature convergence
+	qualified_nodes = filter(lambda node: depth(node, count) > 2, functional_nodes)
+	node = choice(qualified_nodes)	
+	return node
+	
+def getNodes(root, t_or_f=0):	
 	nodes = []
 	loadPaths(root, nodes, t_or_f)
 	return nodes
@@ -110,11 +110,18 @@ def loadPaths(root, path=[], t_or_f=0):
 		path.append(root.value)
 	loadPaths(root.right, path, t_or_f)
 
-#Depth first search
+#Depth First Search
 def DFS(root, target_value):
         if(root == None):
                 return    
         if root.value == target_value:
                 return root
         return DFS(root.left, target_value) or DFS(root.right, target_value)
+
+#Depth of a Tree
+def depth(root):
+	if(root == None):
+		return 0
+	else:
+		return 1 + max(depth(root.left) || depth(root.right))
 
