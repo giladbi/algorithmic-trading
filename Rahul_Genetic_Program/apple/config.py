068f37fbade64b4fcc747fae9aa950fbdb5104bd
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-19 17:20:58 -0700

    Fixed generation number for better results

--- Rahul_Genetic_Program/apple/config.py
+++ Rahul_Genetic_Program/apple/config.py
@@ -1,9 +1,9 @@
 #Rahul Ramakrishnan
 #module: config
 
-population_size = 16   	#Number of trees in the population
-tree_size = 8      	#Number of nodes in a tree
-generations = 20  	#Number of generations
+population_size = 30   	#Number of trees in the population
+tree_size = 10      	#Number of nodes in a tree
+generations = 100  	#Number of generations
 c_probability = .7 	#Crossover probability
 m_probability = .2 	#Mutation probability
 
