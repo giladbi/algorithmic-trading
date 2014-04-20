d99d9528ab7f592557ef9045f4dc9bcfa9237f98
Rahul Ramakrishnan <rahul@plaid.com>
2014-04-19 17:13:10 -0700

    Added sp500 data

--- Rahul_Genetic_Program/apple/scrape.py
+++ Rahul_Genetic_Program/apple/scrape.py
@@ -15,7 +15,8 @@ def getTerminal():
 	terminals = ['apple_open', 'apple_high', 'apple_low', 
 		     'apple_close', 'apple_volume', 'nasdaq_open', 
 		     'nasdaq_high', 'nasdaq_low', 'nasdaq_close', 
-                     'nasdaq_volume']
+                     'nasdaq_volume', 'sp500_open', 'sp500_high', 
+		     'sp500_low', 'sp500_close', 'sp500_volume']
 
 	#Add 10 random coefficients into terminal list
 	#to be used when applying operators on signals
@@ -67,8 +68,7 @@ def getNasdaqData():
 	return nasdaq_data	
 
 
-def getAppleData():
-	#Same thing for nasdaq data as apple
+def getAppleData():	
 	apple = open('/Users/rahulramakrishnan/OneDrive/Projects/algorithmic-trading/Rahul_Genetic_Program/data/test_data/test_apple_stock.txt', 'r')
 	begin_date = datetime.date(2011, 01, 01)
 	end_date = datetime.date(2013, 12, 31)
@@ -89,4 +89,28 @@ def getAppleData():
 			finance_dict["apple_volume"] = float(info[5])
 			apple_data.append(finance_dict)
 	return apple_data		
+
+
+def getSP500Data():
+        sp500= open('/Users/rahulramakrishnan/OneDrive/Projects/algorithmic-trading/Rahul_Genetic_Program/data/sp_500.txt', 'r')
+        begin_date = datetime.date(2011, 01, 01) 
+        end_date = datetime.date(2013, 12, 31) 
+        sp500_data = []
+        for line in sp500:
+                info = line.split(',')
+                date = info[0]
+                year = int(date[:4]) 
+                month = int(date[5:7]) 
+                day = int(date[8:10])
+                current = datetime.date(year, month, day)    
+                finance_dict = {}
+                if current >= begin_date and current <= end_date:
+                        finance_dict["sp500_open"] = float(info[1])
+                        finance_dict["sp500_low"] = float(info[2])
+                        finance_dict["sp500_high"] = float(info[3])
+                        finance_dict["sp500_close"] = float(info[4])
+                        finance_dict["sp500_volume"] = float(info[5])
+                        sp500_data.append(finance_dict)
+        return sp500_data
+
 
